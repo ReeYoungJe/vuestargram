c57bf3db4b47fe78414a8e1dbace86c0ffc98978
륍룡제 <zofhd2@naver.com>
2022-10-27 17:35:09 +0900

    데이터 삽입후 컴포넌트 분할

---
+++ src/components/Container.vue
@@ -0,0 +1,27 @@
+<template>
+  <div>
+    <Post :InstarData="InstarData[0]" />
+    <Post :InstarData="InstarData[1]" />
+    <Post :InstarData="InstarData[2]" />
+
+
+  </div>
+</template>
+
+<script>
+import Post from "@/components/Post";
+export default {
+  name: "Container",
+  components: {
+    Post : Post,
+  },
+  props : {
+    InstarData : Array,
+  }
+
+}
+</script>
+
+<style scoped>
+
+</style>
---
+++ src/components/Post.vue
@@ -0,0 +1,31 @@
+<template>
+
+  <div class="post">
+    <div class="post-header">
+      <div class="profile"></div>
+      <span class="profile-name">{{InstarData.name}}</span>
+    </div>
+    <div class="post-body" :style="{ backgroundImage : 'url(' + InstarData.postImage +')' }"></div>
+    <div class="post-content">
+      <p>{{InstarData.likes}} Likes</p>
+      <p><strong>{{InstarData.name}}</strong> {{InstarData.concat}}</p>
+      <p class="date">May 15</p>
+    </div>
+  </div>
+
+
+
+</template>
+
+<script>
+export default {
+  name: "Post",
+  props : {
+    InstarData : Array,
+  }
+}
+</script>
+
+<style scoped>
+@import "Post.scss";
+</style>
