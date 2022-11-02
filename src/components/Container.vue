f7b520383d50c5357f5615c8e48affa972f7c373
이영제 <zofhd2@naver.com>
2022-11-02 10:42:20 +0900

    Fileter Component 추가 / Props 가져 오기

--- src/components/Container.vue
+++ src/components/Container.vue
@@ -8,11 +8,7 @@
     <div v-if="step === 1">
       <div class="upload-image" :style="{ backgroundImage : `url(${imgData})` }"></div>
       <div class="filters">
-        <div class="filter-1"></div>
-        <div class="filter-1"></div>
-        <div class="filter-1"></div>
-        <div class="filter-1"></div>
-        <div class="filter-1"></div>
+        <FilterBox :Fileter="Fileter"></FilterBox>
       </div>
     </div>
 
@@ -30,69 +26,29 @@
 
 <script>
 import Post from "@/components/Post";
+import FilterBox from "@/components/FilterBox";
 
 export default {
   name: "Container",
+  data(){
+    return{
+      Fileter: [ "aden", "_1977", "brannan", "brooklyn", "clarendon", "earlybird", "gingham", "hudson",
+      "inkwell", "kelvin", "lark", "lofi", "maven", "mayfair", "moon", "nashville", "perpetua",
+      "reyes", "rise", "slumber", "stinson", "toaster", "valencia", "walden", "willow", "xpro2"]
+    }
+  },
   components: {
+    FilterBox,
     Post: Post,
   },
   props: {
     InstarData: Object,
     step: Number,
     imgData : String,
-  }
-
+  },
 }
 </script>
 
 <style scoped>
-.upload-image {
-  width: 100%;
-  height: 450px;
-  background: cornflowerblue;
-  background-size: cover;
-}
-
-.filters {
-  overflow-x: scroll;
-  white-space: nowrap;
-}
-
-.filter-1 {
-  width: 100px;
-  height: 100px;
-  background-color: cornflowerblue;
-  margin: 10px 10px 10px auto;
-  padding: 8px;
-  display: inline-block;
-  color: white;
-  background-size: cover;
-}
-
-.filters::-webkit-scrollbar {
-  height: 5px;
-}
-
-.filters::-webkit-scrollbar-track {
-  background: #f1f1f1;
-}
-
-.filters::-webkit-scrollbar-thumb {
-  background: #888;
-  border-radius: 5px;
-}
-
-.filters::-webkit-scrollbar-thumb:hover {
-  background: #555;
-}
-
-.write-box {
-  border: none;
-  width: 90%;
-  height: 100px;
-  padding: 15px;
-  margin: auto;
-  display: block;
-  outline: none;
-}
+ @import "Container.scss";
 </style>
---
+++ src/components/FilterBox.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="filter-item"></div>
+</template>
+
+<script>
+
+export default {
+  name: "FilterBox",
+  props:{
+    Fileter : Array,
+  },
+}
+</script>
+
+<style scoped>
+ @import "FilterBox.scss";
+</style>
